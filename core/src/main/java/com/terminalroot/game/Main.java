d8f900a577268653be426e312dcec1da017596a1
MarcoSoaresL <contadofus501@gmail.com>
2025-10-23 20:47:30 -0300

    testeses

--- core/src/main/java/com/terminalroot/game/Main.java
+++ core/src/main/java/com/terminalroot/game/Main.java
@@ -1,18 +1,25 @@
 package com.terminalroot.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.sun.org.apache.xerces.internal.impl.dtd.DTDGrammarBucket;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
+public class Main implements ApplicationListener {
     Texture backgroundTexture;
     Texture bucketTexture;
     Texture dropTexture;
@@ -23,6 +30,15 @@ public class Main extends ApplicationAdapter {
     FitViewport viewport;
 
     Sprite bucketSprite;
+
+    Vector2 touchPos;
+
+    Array<Sprite> dropSprites;
+
+    float dropTimer;
+
+    Rectangle bucketRectangle;
+    Rectangle dropRectangle;
 
     @Override
     public void create() {
@@ -37,6 +53,17 @@ public class Main extends ApplicationAdapter {
 
         bucketSprite = new Sprite(bucketTexture);
         bucketSprite.setSize(1,1);
+
+        touchPos = new Vector2();
+
+        dropSprites = new Array<>();
+
+        bucketRectangle = new Rectangle();
+        dropRectangle = new Rectangle();
+
+        music.setLooping(true);
+        music.setVolume(0.5f);
+        music.play();
     }
 
     @Override
@@ -45,11 +72,58 @@ public class Main extends ApplicationAdapter {
     }
 
     private void input(){
+        float speed = 4f;
+        float delta = Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            bucketSprite.translateX(speed * delta);
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            bucketSprite.translateX(-speed * delta);
+        }
+
+        if (Gdx.input.isTouched()){
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
+            viewport.unproject(touchPos);
+            bucketSprite.setCenterX(touchPos.x);
+        }
 
     }
 
     private void logic(){
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
 
+        bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(), 0, worldWidth));
+
+        float bucketWidth = bucketSprite.getWidth();
+        float bucketHeight = bucketSprite.getHeight();
+
+        bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(),0,worldWidth - bucketWidth));
+
+        float delta = Gdx.graphics.getDeltaTime();
+
+        bucketRectangle.set(bucketSprite.getX(), bucketSprite.getY(), bucketWidth, bucketHeight);
+
+        for(int i  = dropSprites.size - 1; i >= 0; i--){
+            Sprite dropSprite = dropSprites.get(i);
+            float dropWidth = dropSprite.getWidth();
+            float dropHeight = dropSprite.getHeight();
+
+            dropSprite.translateY((-2f * delta));
+            dropRectangle.set(dropSprite.getX(), dropSprite.getY(), dropWidth, dropHeight);
+
+            if (dropSprite.getY() < -dropHeight) dropSprites.removeIndex(i);
+            else if(bucketRectangle.overlaps(dropRectangle)){
+                dropSprites.removeIndex(i);
+                dropSound.play();
+            }
+        }
+
+        dropTimer += delta;
+        if (dropTimer > 1f){
+            dropTimer = 0;
+            createDroplet();
+        }
     }
 
     private void draw(){
@@ -64,7 +138,24 @@ public class Main extends ApplicationAdapter {
         spriteBatch.draw(backgroundTexture, 0,0,worldWidth, worldHeight);
         bucketSprite.draw(spriteBatch);
 
+        for(Sprite dropSprite : dropSprites){
+            dropSprite.draw(spriteBatch);
+        }
+
         spriteBatch.end();
+    }
+
+    private void createDroplet(){
+        float dropWidth = 1;
+        float dropHeight = 1;
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        Sprite dropSprite = new Sprite((dropTexture));
+        dropSprite.setSize(dropWidth, dropHeight);
+        dropSprite.setX(MathUtils.random(0f, worldWidth - dropWidth));
+        dropSprite.setY(worldHeight);
+        dropSprites.add(dropSprite);
     }
 
     @Override
@@ -75,6 +166,21 @@ public class Main extends ApplicationAdapter {
     }
 
     @Override
+    public void pause(){
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
     public void dispose() {
+        backgroundTexture.dispose();
+        dropSound.dispose();
+        music.dispose();
+        dropTexture.dispose();
+        bucketTexture.dispose();
     }
 }
