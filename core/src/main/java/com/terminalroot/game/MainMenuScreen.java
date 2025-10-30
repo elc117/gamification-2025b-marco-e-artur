2e20fa46711eb2a2e9b428ccb0ffc27d22cebf8d
MarcoSoaresL <contadofus501@gmail.com>
2025-10-30 08:29:31 -0300

    Botao funcionando, Transição tela inicial -> tela jogo funcionando

--- core/src/main/java/com/terminalroot/game/MainMenuScreen.java
+++ core/src/main/java/com/terminalroot/game/MainMenuScreen.java
@@ -4,20 +4,31 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import org.w3c.dom.Text;
+import com.badlogic.gdx.graphics.
 
 public class MainMenuScreen implements Screen {
 
     final Drop game;
     final Controle_Diagrama_Estados controle;
+    private SpriteBatch batch;
+    Texture tela_principal;
+
+    // Implementação do personagem, dá pra trocar isso depois
+
 
     public MainMenuScreen(final Drop game, Controle_Diagrama_Estados controle) {
         this.game = game;
         this.controle = controle;
+        this.batch = new SpriteBatch();
     }
 
     @Override
     public void show() {
+        tela_principal = new Texture("mapafinal.png");
     }
 
     @Override
@@ -25,10 +36,9 @@ public class MainMenuScreen implements Screen {
         ScreenUtils.clear(Color.DARK_GRAY);
 
         game.batch.setProjectionMatrix(game.viewport.getCamera().combined);
-        game.batch.begin();
-        game.font.draw(game.batch, "Bem-vindo ao Drop!", 1, 3);
-        game.font.draw(game.batch, "Toque ou pressione ENTER para jogar.", 1, 2);
-        game.batch.end();
+        batch.begin();
+        batch.draw(tela_principal, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
 
         if (Gdx.input.isTouched() || Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
             game.setScreen(new GameScreen(game));
