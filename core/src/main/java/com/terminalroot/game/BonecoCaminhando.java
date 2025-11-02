03051471a0b591488acafabeb0cbe4f4139c7459
MarcoSoaresL <contadofus501@gmail.com>
2025-11-02 18:12:51 -0300

    BONECO TA MEXENDO FINALMENTEEEEEEEEE

--- core/src/main/java/com/terminalroot/game/BonecoCaminhando.java
+++ core/src/main/java/com/terminalroot/game/BonecoCaminhando.java
@@ -31,39 +31,38 @@ public class BonecoCaminhando {
     }
 
     private void initAnimations() {
-        Texture spreedshetCaminhadaBaixo = new Texture("hero/Eni/baixo/RecortadaBaixo.png");
+        Texture spreedshetCaminhadaBaixo = new Texture("hero/Eni/baixo/RecortadaBaixoCerto.png");
         int frameColsBaixo = 4;
         int frameRowsBaixo = 4;
 
         int frameWidthBaixo = spreedshetCaminhadaBaixo.getWidth() / frameColsBaixo;
         int frameHeightBaixo = spreedshetCaminhadaBaixo.getHeight() / frameRowsBaixo;
 
-        Texture spreedshetCaminhadaCima = new Texture("hero/Eni/cima/RecortadaCima.png");
+        Texture spreedshetCaminhadaCima = new Texture("hero/Eni/cima/RecortadaCimaCerto.png");
         int frameColsCima = 4;
         int frameRowsCima = 4;
 
         int frameWidthCima = spreedshetCaminhadaCima.getWidth() / frameColsCima;
         int frameHeightCima = spreedshetCaminhadaCima.getHeight() / frameRowsCima;
 
-        Texture spreedshetCaminhadaEsquerda = new Texture("hero/Eni/esquerda/RecortadaEsquerda.png");
+        Texture spreedshetCaminhadaEsquerda = new Texture("hero/Eni/esquerda/RecortadaEsquerdaCerto.png");
         int frameColsEsquerda = 4;
         int frameRowsEsquerda = 4;
 
         int frameWidthEsquerda = spreedshetCaminhadaEsquerda.getWidth() / frameColsEsquerda;
         int frameHeightEsquerda = spreedshetCaminhadaEsquerda.getHeight() / frameRowsEsquerda;
 
-        Texture spreedshetCaminhadaDireita = new Texture("hero/Eni/direita/Recortada_direita.png");
+        Texture spreedshetCaminhadaDireita = new Texture("hero/Eni/direita/RecortadaDireitaCerto.png");
         int frameColsDireita= 4;
         int frameRowsDireita = 4;
 
-        int frameWidthDireita = spreedshetCaminhadaEsquerda.getWidth() / frameColsDireita;
-        int frameHeightDireita = spreedshetCaminhadaEsquerda.getHeight() / frameRowsDireita;
+        int frameWidthDireita = spreedshetCaminhadaDireita.getWidth() / frameColsDireita;
+        int frameHeightDireita = spreedshetCaminhadaDireita.getHeight() / frameRowsDireita;
 
         TextureRegion[][] MovimentoCima = TextureRegion.split(spreedshetCaminhadaCima, frameWidthCima, frameHeightCima);
         TextureRegion[][] MovimentoBaixo = TextureRegion.split(spreedshetCaminhadaBaixo, frameWidthBaixo, frameHeightBaixo);
         TextureRegion[][] MovimentoEsquerda = TextureRegion.split(spreedshetCaminhadaEsquerda,frameWidthEsquerda, frameHeightEsquerda);
         TextureRegion[][] MovimentoDireita = TextureRegion.split(spreedshetCaminhadaDireita, frameWidthDireita, frameHeightDireita);
-
 
         TextureRegion[] DirBaixo = new TextureRegion[16];
         TextureRegion[] DirEsq = new TextureRegion[16];
@@ -119,6 +118,7 @@ public class BonecoCaminhando {
     public void update(float deltaTime, String direction, boolean movimento){
         if(!direction.equals(direcao)){
             direcao = direction;
+            stateTime = 0;
             switch (direction){
                 case "cima":
                     AnimacaoAtual = AnimacaoCaminhaCima;
